160ea63262c573ee9861fda8286e874ed1d2c108
JosephRidge <josephridge18@gmail.com>
2026-05-22 16:26:11 +0300

    update

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+# this is a comment
+
+"""
+ This is a multi-line string, 
+ can be used as a multiline comment as well
+"""
+
+# standard input => allows you to input information into the program
+username = input("Enter your username: ") # input function
+# standard ouput 
+print(username)
+# standard errors
+print(j) # this evokes a NameErro since it has not been defined
