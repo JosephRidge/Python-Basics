1f39c14034715d690240586481dc9d7f8e637941
JosephRidge <josephridge18@gmail.com>
2026-06-02 15:10:55 +0300

    update

--- lesson4.1.py
+++ lesson4.1.py
@@ -24,6 +24,26 @@ output = fruits[2] #accessing via an index
 output = fruits.count("apple") # how many of occurence 
 output = fruits.index("pineapples")
 
+# dictionary (you are represenitng real world things eg person)
+    # - key : value pair
+    # key is often in string form and can be numbers
+    # we use the key to access the values
+
+student = {
+    "name":"John Doe", 
+    "age": 12,
+    "course": "SWE1"
+}
+output = student.get("name") # example of a dictionary method that returns the value of a particular key
+output  = student.copy()
+# output = student.clear() # delete items
+examiner = {"John Doe", "Peter Parker" , "Noir"}
+units = 0
+output = dict.fromkeys(examiner, units) # returns to us a dictionary combined from a list of items and a value to place in each 
+ouput  = output.clear() # delete items
+output = student.items() # returns to us a tuple composed of the data key-value pair
+output = student.keys() # we get the keys 
+
 print("==============================================")
 print(output)
 print("==============================================")
