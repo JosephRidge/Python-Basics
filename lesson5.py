021c4ea4d422cbb2c028aabc9450f0b133e165c6
JosephRidge <josephridge18@gmail.com>
2026-06-02 16:12:26 +0300

    update

---
+++ lesson5.py
@@ -0,0 +1,10 @@
+"""
+    Control flows: 
+        - for loop
+        - while loop
+        - do.. while loop
+    - Functions:
+        - parameterized
+        - non-parameterized
+        - anonymous function (lambda functions)
+"""
