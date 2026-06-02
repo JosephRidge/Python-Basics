1a15c74600b0be9c7c3178388170066bb43e0303
JosephRidge <josephridge18@gmail.com>
2026-06-02 16:29:06 +0300

    update

--- lesson5.py
+++ lesson5.py
@@ -8,3 +8,29 @@
         - non-parameterized
         - anonymous function (lambda functions)
 """
+
+# loops => running iteration:
+
+fruits = ["apples", "mangoes", "bananas", "water melon", "kiwi", "pineapples"] # array list
+
+# for loop
+for fruit in fruits:
+    # print(fruit)
+    pass # this temporarily sets the process ot have no logic until engineer write it. 
+
+    #  nested loop
+for fruit in fruits:
+    for letter in fruit.upper():
+        # print(letter)
+        pass # this temporarily sets the process ot have no logic until engineer write it. 
+
+fruits_two = fruits.copy()
+
+while (len(fruits_two) <= len(fruits)):
+    print(fruits)
+    print(fruits_two)
+    # fruits.pop() # it will pop last item
+    fruits_two.append("guavas")
+    print(fruits_two)
+
+
